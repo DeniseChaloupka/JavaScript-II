049bddc64c921a4e87d11afeb97c0a592c014acd
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-05-24 21:01:11 -0500

    had fun with this one

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,30 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+let monster = "Monsters";
+let trained = "trained";
+let lineThree = "no commin' back"
+
+function lyricLineOne() {
+  const realMonster = "are real";
+  console.log(`Cause my ${monster} ${realMonster}`);
+
+  function lyricLineTwo() {
+    const verylyrics = "how to kill,";
+    console.log(`and they're ${trained} ${verylyrics}`);
+
+    function lyricLineThree() {
+      const lyrical = "they just laughed"
+      console.log(`And there's ${lineThree} and ${lyrical} at how I feel`);
+
+    }
+
+    lyricLineThree();
+  }
+
+  lyricLineTwo();
+}
+lyricLineOne();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -8,7 +32,22 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+
+  return function () {
+    count++;
+    return count;
+  }
+
 };
+
+const newCounter = counter();
+
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
